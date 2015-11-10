7b3743a1ed7694f22905dec6ee4c8606631e9992
Stivinson <stivinson02@gmail.com>
2015-11-10 11:52:44 -0500

    Tutorial SQLite v1.1

--- app/src/main/java/com/stivinsonmartinez/contactsqlite/MainActivity.java
+++ app/src/main/java/com/stivinsonmartinez/contactsqlite/MainActivity.java
@@ -2,6 +2,7 @@ package com.stivinsonmartinez.contactsqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost= (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("creator");
+        tabSpec.setContent(R.id.Contact_Creator);
+        tabSpec.setIndicator("Creator");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("list");
+        tabSpec.setContent(R.id.Contact_List);
+        tabSpec.setIndicator("List");
+        tabHost.addTab(tabSpec);
+
     }
 }
